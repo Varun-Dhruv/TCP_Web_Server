e4c1e75569f3d9c00e17eb954a852eafdf9aba0d
Varun Koranne <88835482+Varun-Dhruv@users.noreply.github.com>
2023-01-21 06:38:21 +0530

    Added props

--- src/server_linux.cpp
+++
@@ -1,7 +0,0 @@
-#include <http_tcpServer_linux.h>
-int main()
-{
-    using namespace http;
-    TcpServer server = TcpServer();
-    return 0;
-}
---
+++ src/windows/server_win.cpp
@@ -0,0 +1,7 @@
+#include <http_tcpServer_win.h>
+int main()
+{
+    using namespace http;
+    TcpServer server = TcpServer();
+    return 0;
+}
